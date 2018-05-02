7b1e1fcba4f1f784729f5a55bb6ebe024434b8ef
jdbinkley6 <jdbinkley6@gmail.com>
2018-05-02 19:37:43 -0400

    Updated today's notes

--- Week01/Tasks/bootstrap/javascript/myjs.js
+++ Week01/Tasks/bootstrap/javascript/myjs.js
@@ -1,72 +1,86 @@
 let addCount = 0;
 function appendText(){
-    document.getElementById("para1").innerHTML += ("\nAdded Stuff! #" + ++addCount);
+    document.getElementById("para1").innerHTML += ("<br>Added Stuff! #" + ++addCount);
+
 }
 
-function logIt(){
-    console.log("Logged it!: " + ++addCount);
+function logit(){
+    console.log('Logged It!: ' + ++addCount);
 }
 
-function write(){
+function writeToPage(){
     document.write("<h1>Document.write!</h1>");
 }
 
 function changeWell(){
     var el = document.getElementById("magicWell");
+
     var random = Math.floor((Math.random()*4));
     switch(random){
         case 0:
-        el.setAttribute("style", "background-color: red");
-        break;
+            el.setAttribute("style", "background-color: red");
+            break;
         case 1:
-        el.setAttribute("style", "background-color: blue");
-        break;
+            el.setAttribute("style", "background-color: blue");
+            break;
         case 2:
-        el.setAttribute("style", "background-color: yellow");
-        break;
+            el.setAttribute("style", "background-color: yellow");
+            break;
         case 3:
-        el.setAttribute("style", "background-color: green");
-        break;
+            el.setAttribute("style", "background-color: green");
+            break;
     }
 }
 
 var empCounter = 0;
 function addEmp(){
-    // Store input values
-    // .value represents content of input box
+    // Store input values first, value represents content of input box
     var empName = document.getElementById("empName").value;
+
     var favDog = document.getElementById("favDog").value;
 
     // Create nodes for the text to go in
     var nameText = document.createTextNode(empName); // input1
-    var favText = document.createTextNode(favDog); // input2
+    var favText = document.createTextNode(favDog); // input 2
     var empID = document.createTextNode(++empCounter); // counter
     var del = document.createTextNode("X"); // X
 
-    // Create elements to store information
-    var td1 = document.createElement('td'); // <td></td>
-    var td2 = document.createElement('td'); // <td></td>
-    var td3 = document.createElement('td'); // <td></td>
-    var td4 = document.createElement('td');
-    
-    var delBut = document.createElement('button'); //<button></button>
+    // Create the elements to store our information
+    var td1 = document.createElement("td"); // <td></td>
+    var td2 = document.createElement("td"); // <td></td>
+    var td3 = document.createElement("td"); // <td></td>
+    var td4 = document.createElement("td"); // <td></td>
 
-    delBut.setAttribute("onclick", "removeRow(" + empCounter + ")"); // <button onclick="removeRow(empCounter)"></button>
-    delBut.setAttribute('style', 'color: red'); // <button style="color: red"></button>
-    delBut.appendChild(del); // <button onclick="removeRow(empCounter)"></button>
-    td1.appendChild(empID);
-    // <td>empID</td>
-    td2.appendChild(nameText);
-    // <td>empName</td>
-    td3.appendChild(favText);
-    td4.appendChild(delBut);
+    var delBut = document.createElement("button"); // <button></button>
 
-    // Create new row
-    var row = document.createElement("tr"); // <tr></tr>
+    // <button onclick="removeRow(empCounter)"></button>
+    delBut.setAttribute("onclick", "removeRow(" + empCounter + ")");
+
+    // <button onclick="removeRow(empCounter)" style="color: red"></button>
+    delBut.setAttribute("style", "color: red");
+
+    // <button onclick="removeRow(empCounter)" style="color: red">X</button>
+    delBut.appendChild(del);
+
+    td1.appendChild(empID); // <td>empID</td>
+    td2.appendChild(nameText); // <td>empName</td>
+    td3.appendChild(favText); // <td>favDog</td>
+    td4.appendChild(delBut); // <td><button onclick="removeRow(empCounter)" style="color: red">X</button></td>
+
+    // Finally we create our NEW row
+    var row = document.createElement('tr'); // <tr></tr>
     row.appendChild(td1);
     row.appendChild(td2);
     row.appendChild(td3);
     row.appendChild(td4);
+    /*
+        <tr>
+            <td>empID</td>
+            <td>empName</td>
+            <td>favDog</td>
+            <td><button onclick="removeRow(empCounter)" style="color: red">X</button></td>
+        </tr>
+    */
 
     row.setAttribute("id", empCounter);
 
@@ -79,8 +93,35 @@ function removeRow(x){
     document.getElementById(x).remove();
 }
 
-/* Window.onload is a property that triggers a callback function to execute once a
-webpage has been fully loaded */
+/*
+    window.onload is a property that triggers a callback function to execute once a
+    webpage has been fully loaded
+*/
+
 window.onload = function(){
     document.getElementById("para1").innerHTML = "TEST";
+
+    var d1 = document.getElementById("d1");
+    var d2 = document.getElementById("d2");
+    var d3 = document.getElementById("d3");
+
+    // addEventListener passes 3 arguments
+    // event, callback, useCapture <- this is set to false by default
+    // We can use event propagation to prevent bubbling/capturing in its entirety
+
+    d1.addEventListener("click", d1click);
+    d2.addEventListener("click", d2click);
+    d3.addEventListener("click", d3click);
+
+    function d1click(event){
+        window.alert("d1 clicked");
+    }
+
+    function d2click(event){
+        window.alert("d2 clicked");
+    }
+
+    function d3click(event){
+        window.alert("d3 clicked");
+    }
 }
