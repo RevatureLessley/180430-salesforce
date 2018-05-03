f4c81b82cd9a5125d615a7e1bcb191dae5cda3a9
xenoazn <programmsamm@gmail.com>
2018-05-02 22:46:40 -0400

    Day 3

---
+++ Week01/_InClassExamples/sExamples/Tests/Javascript/my.js
@@ -0,0 +1,15 @@
+﻿let addCount = 0;
+function appendText() {
+    document.getElementById("para1").innerHTML += "\nAdded Stuff!" + ++addCount<br>;
+}
+
+function logit() {
+    console.log('Logged it!: ' + ++addCount);
+}
+function writeToPage() {
+    document.write("<h1>Success</h1>");
+}
+function msg() {
+    alert("Hello Javatpoint");
+}  
+
