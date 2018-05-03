07a2453ac6151a9ec6e09ea0bbbcc7ed7464075a
StrangerCoug <strangercoug@hotmail.com>
2018-05-03 13:22:54 -0400

    Got the fizzbuzz working!

--- Week01/Tasks/FizzBuzz/javascript/fizzbuzz.js
+++ Week01/Tasks/FizzBuzz/javascript/fizzbuzz.js
@@ -1,3 +1,25 @@
 function count() {
+	var max = document.getElementById("maxnum").value;
+	console.log("Number " + max + " submitted.");
 	
+	document.getElementById("numbers").innerHTML = ""; // Clear previous
+	
+	// All console.log() statements are for me to debug my code.
+
+	
+	for (var i = 1; i <= max; i++) {
+		if (i % 15 == 0) {
+			document.getElementById("numbers").innerHTML += "fizzbuzz ";
+		}
+		else if (i % 3 == 0) {
+			document.getElementById("numbers").innerHTML += "fizz ";
+		}
+		else if (i % 5 == 0) {
+			document.getElementById("numbers").innerHTML += "buzz ";
+		}
+		else {
+			document.getElementById("numbers").innerHTML += (i + " ");
+		}
+	}
+	console.log("Counting complete.");
 }
