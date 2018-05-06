6a84df0747d8ae8b0c11b3a7bc9c397b93bdf14d
knmartin <knmartin@uga.edu>
2018-05-06 16:44:36 -0400

    Mini-project Submission

--- Week01/Tasks/fizzbuzz.js
+++ Week01/Tasks/fizzbuzz.js
@@ -1,12 +1,13 @@
 
 function fizzBuzz(){
-    document.getElementById("para1").innerHTML = "";
-    var numRangeMin = Number(document.getElementById("numRangeMin").value);
+    document.getElementById("para1").innerHTML = "";//when Submit is pressed, this clears the text output everytime 
+                                                    //to ensure nothing is left from past uses
+    var numRangeMin = Number(document.getElementById("numRangeMin").value); //variables to store input are casted to reduce errors
     var numRangeMax = Number(document.getElementById("numRangeMax").value);
     var minText = document.createTextNode(numRangeMin);//input1
     var maxText = document.createTextNode(numRangeMax);//input2
 
-    for (var i=numRangeMin; i <= numRangeMax; i++)
+    for (var i=numRangeMin; i <= numRangeMax; i++)//this for loop is the algorithm that determines what is printed
     {
         if (i % 15 == 0)
             document.getElementById("para1").innerHTML += "<br>FizzBuzz";
