629f14940e00a1184a4e31c9cb44e521fcb3944a
xenoazn <programmsamm@gmail.com>
2018-05-06 23:04:11 -0400

    Website

--- Week01/Tasks/miniProject/Javascript/myJS.js
+++ Week01/Tasks/miniProject/Javascript/myJS.js
@@ -1,1 +1,63 @@
-﻿
+﻿function myTable() {
+    //Javascript function to run and make a table and hide the list system while deleting the table every time the link is clicked. 
+    var list = document.getElementById("hobbyList");
+   // list.parentNode.removeChild(list);
+    if (list.style.display === "none") {
+        list.style.display = "block";
+        var delTable = document.getElementById("myTable");
+        delTable.parentNode.removeChild(delTable);
+
+    } else {
+        list.style.display = "none";
+        var xTable = document.createElement("TABLE");
+        xTable.setAttribute("id", "myTable");
+        document.body.appendChild(xTable);
+
+        var tRow = document.createElement("TR");
+        tRow.setAttribute("id", "myTr");
+        document.getElementById("myTable").appendChild(tRow);
+        var hob2 = document.createElement("TR");
+        hob2.setAttribute("id", "myTr2");
+        document.getElementById("myTable").appendChild(hob2);
+        var hob3 = document.createElement("TR");
+        hob3.setAttribute("id", "myTr3");
+        document.getElementById("myTable").appendChild(hob3);
+
+        var hobtd1 = document.createElement("TD");
+        var hobtd2 = document.createElement("TD");
+        var hobtd3 = document.createElement("TD");
+        var hobb1 = document.createTextNode("1. Computer Hardware");
+        var hobb2 = document.createTextNode("2. Chess");
+        var hobb3 = document.createTextNode("3. Piano");
+        var img1 = document.createElement("TD");
+        var img2 = document.createElement("TD");
+        var img3 = document.createElement("TD");
+        var imgHardware = document.getElementById("comHardware");
+        var imgChess = document.getElementById("chess");
+        var imgPiano = document.getElementById("piano");
+        hobtd1.appendChild(hobb1);
+        document.getElementById("myTr").appendChild(hobtd1);
+        hobtd2.appendChild(hobb2);
+        document.getElementById("myTr2").appendChild(hobtd2);
+        hobtd3.appendChild(hobb3);
+        document.getElementById("myTr3").appendChild(hobtd3);
+        img1.appendChild(imgHardware);
+        document.getElementById("myTr").appendChild(img1);
+        img2.appendChild(imgChess);
+        document.getElementById("myTr2").appendChild(img2);
+        img3.appendChild(imgPiano);
+        document.getElementById("myTr3").appendChild(img3);
+        var divTable = document.getElementById("hobbyDiv");
+        divTable.appendChild(xTable);
+    }
+    
+    
+
+        
+
+        
+
+
+  
+
+}
--- Week01/_InClassExamples/MyStaticWebpage/javascript/ajax.js
+++ Week01/_InClassExamples/MyStaticWebpage/javascript/ajax.js
@@ -1,54 +1,10 @@
 function retrieve(){
-<<<<<<< HEAD
-    /*
-    XMLHttpRequest object is the object we use in JAX in order to communicate with server API endpoints in order to send a request, and get a response
-    with the data we wanted.
-
-    XMLHttpRequest objects have Five states it can be in.
-    These states are numbered from 0-4.
-    0- Request is not configured yet.
-       -We just created our XMLHttpRequest object:
-    1. - Request has been properly configured
-       -We have called the .open() method on our object.
-    2. The request has been sent to a server
-      - We called the .send() method.
-    3.-Request is recieved and being processed.
-      -Communication with the server has been established.
-      -Yet we have not received a response yet.
-    4.Request has been sent, and a proper response has been received.
-     -request/response lifecycle is complete.
-
-*/
-    var pkid = document.getElementById("pkmnid").value;
-
-    var xhr = new XMLHttpRequest(); //State 0
-    var url = "https://pokeapi.co/api/v2/pokemon/" + pkid + "/";
-    console.log("XHR created, state: " + xhr.readyState);
-    xhr.onreadystatechange = function() {
-        console.log("Ready State is now " + xhr.readyState);
-        if (xhr.onreadystate == 4 && xhr.status == 200)
-        {
-            //Run this code only if we guranatee the request is finished.
-            //AND it was sucessful. Status 200 = OK
-        }
-        else if(xhr.readyState==4 && xhr.status != 200){
-            console.log("ERROR");
-        }
-    }
-    xhr.open("GET", url); //State 1
-    
-
-    xhr.send(); //STATE 2
-
-
-=======
     document.getElementById("pokediv").innerHTML=
         "<img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif'>";
 /*
     XMLHttpRequest object is the object we use in AJAX in order to 
     communicate with server API endpoints in order to send a request,
     and get a response with the data we wanted.
-
     XMLHttpRequest objects have FIVE states it can be in.
     These states are numbered from 0-4.
     0 - Request is not configured yet.
@@ -158,5 +114,4 @@ function retrieve(){
     console.log("XHR created, state: " + xhr.readyState);
     xhr.open("GET", url); //STATE 1
     xhr.send(); //STATE 2
->>>>>>> e603dae6145497465b7f6f7b5dd5c49840d7e60b
 }
