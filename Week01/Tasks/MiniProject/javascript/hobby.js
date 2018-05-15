04a6c0b90d5c7a4897e79c8e6833fb1a0e369b70
Kyle Chiu <kchiu132@gmail.com>
2018-05-15 00:08:19 -0400

    FINAL FINAL FINAL FINAL update

--- Week01/Tasks/MiniProject/javascript/hobby.js
+++ Week01/Tasks/MiniProject/javascript/hobby.js
@@ -2,13 +2,13 @@ var intvl;
 
 // animation speeds in changes-per-second.
 const FPS1 = 1000;
-const FPS2 = (1000/2);
-const FPS4 = (1000/4);
-const FPS20 = (1000/20);
-const FPS24 = (1000/24);
-const FPS30 = (1000/30);
-const FPS60 = (1000/60);
-const FPS120 = (1000/120);
+const FPS2 = (1000 / 2);
+const FPS4 = (1000 / 4);
+const FPS20 = (1000 / 20);
+const FPS24 = (1000 / 24);
+const FPS30 = (1000 / 30);
+const FPS60 = (1000 / 60);
+const FPS120 = (1000 / 120);
 
 $(document).ready(function () {
 
@@ -17,19 +17,21 @@ $(document).ready(function () {
     $("#please").mouseout(stp);
 
     //surprise image change when you mouse over this well. changes back once you mouse out
-    $("#i1").mouseover(function(){
-        $(this).attr("src","CSS/Assets/Shrek Roar.png");
+    $("#i1").mouseover(function () {
+        $(this).attr("src", "CSS/Assets/Shrek Roar.png");
     });
-    $("#i1").mouseout(function(){
+    $("#i1").mouseout(function () {
         $(this).attr("src", "CSS/Assets/n64.jpg");
     });
 
-    $("#i2").mouseover(function(){
+    $("#i2").mouseover(function () {
         $(this).attr("src", "CSS/Assets/Shrek Smile.png");
     });
-    $("#i2").mouseout(function(){
+    $("#i2").mouseout(function () {
         $(this).attr("src", "CSS/Assets/Wheat Bread Man.png");
     });
+
+    ani2();
 
 });
 
@@ -40,10 +42,24 @@ function changeColor() {
     i = (i + 1) % colors.length;
 };
 
-function ani(){
-    intvl = setInterval(function(){changeColor();}, FPS4); 
+var j = 0;
+function showImg() {
+    var images = ["CSS/Assets/LoL Logo.png", "CSS/Assets/Jack Frost.gif", "CSS/Assets/Clicker Heroes.gif"];
+    var paste = "<img src='";
+    paste += images[j];
+    paste += "'>";
+    $("#gaming").html(paste);
+    j = (j + 1) % images.length;
 }
 
-function stp(){
+function ani() {
+    intvl = setInterval(function () { changeColor(); }, FPS4);
+}
+
+function ani2(){
+    setInterval(function(){showImg();},FPS1);
+}
+
+function stp() {
     clearInterval(intvl);
 }
